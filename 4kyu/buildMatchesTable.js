fb0815b196b8d57c6d42272003ccbea7bc60ad1d
MisterScriptster <mistersinister1@protonmail.com>
2019-10-16 11:57:25 +0200

    refactored, done

--- 4kyu/buildMatchesTable.js
+++ 4kyu/buildMatchesTable.js
@@ -1,58 +1,44 @@
-//round robin tournament simulator, everyone plays with everyone
+// https://www.codewars.com/kata/organize-a-round-robin-tournament/train/javascript
+
+// You are organizing a soccer tournament, so you need to build a matches table.
+// The tournament is composed by 20 teams. It is a round-robin tournament (all-play-all), so it has 19 rounds, and each team plays once per round.
+// Each team confront the others once in the tournament (each match does not repeat in the tournament).
+// Your mission is to implement a function "buildMatchesTable" that receives the number of teams (always a positive and even number) and returns a matrix. 
+// Each line of the matrix represents one round. Each column of the matrix represents one match. 
+// The match is represented as an array with two teams. Each team is represented as a number, starting from 1 until the number of teams.
 
 function buildMatchesTable(numberOfTeams) {
     
-    let round = 0;
     let finalTable = []
     let minorTable = []
-    let numbers = [1]
+    let roundTable = []
+    let half = Math.floor(numberOfTeams/2)
 
-    for(let i= 0; i < numberOfTeams-1; i++){
-        minorTable.push([])
-        finalTable.push([])
-        numbers.push(i+2)
-    }
-    
-    let robin = 0;
-   
+    for(let i = 0; i < numberOfTeams; i++)
+    {   
+        minorTable.push(i+1); 
+    }    
+    memory = minorTable[1]
 
-    for(let i = 0 + round; i < 1; i++){
-        for(let j = 1+round; j < numberOfTeams; j++){
-            
-            minorTable[robin].push([i+1, j+1])
-            finalTable[robin].push(i+1, j+1)
-            robin++
-            if(robin == numberOfTeams-1) {
-                robin = 0   
-            };
-        }
-        round++
-    }
+    for(let i = 0; i < numberOfTeams-1; i++)
+    {
+        
+        const moveToLastPostion = minorTable.splice(minorTable.length-1, 1)
+        minorTable.splice(1, 0, moveToLastPostion[0])
 
-    
-    let taken =[]
-
-    for(let i = 1; i < finalTable.length+1; i++){
-        for(let j = 0; j < 2; j++){
-
-           taken.push(finalTable[i-1][j])
-            
-        }
-        console.log(taken)
-        for(let k = 0; k < taken.length; k++){
-            
+        for(let j =0; j < half; j++){
+        roundTable.push([minorTable[j], minorTable[minorTable.length-j-1]]) 
         }
 
-        taken = []
+        finalTable.push(roundTable)
+        roundTable = []
+        }
+
+        return finalTable
+            
     }
 
 
-
-    
-    return finalTable
-}
-
-
-const result = buildMatchesTable(10)
+const result = buildMatchesTable(8)
 
 console.log(result)
